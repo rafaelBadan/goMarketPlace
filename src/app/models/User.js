4605983582eb1441202e532ea0c5ed7021236c27
rafaelBadan <rafael.badan@gmail.com>
2019-02-27 10:22:46 -0300

    initial commit

---
+++ src/app/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  this.password = await bcrypt.hash(this.password, 8)
+})
+
+module.exports = mongoose.model('User', UserSchema)
